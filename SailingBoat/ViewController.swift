de7dfd73201e2284b45fb80284922115c402435c
Liliya <lilyeka@gmail.com>
2019-06-13 12:44:17 +0500

    Initial Commit

---
+++ SailingBoat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SailingBoat
+//
+//  Created by Лилия Левина on 13/06/2019.
+//  Copyright © 2019 Лилия Левина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
